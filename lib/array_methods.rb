d72c00a34f4e6b443ad947583e3fcc908b712307
Leo Kim <sotysk@gmail.com>
2020-02-21 01:36:10 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,25 +1,8 @@
+require 'pry'
 def using_include(array, element)
-  using_include = ["wow", "I", "am", "really", "learning", "arrays!"]
-  element1 = "wow"
-  using_include.include?("wow")
+  array.include?(element)
 end
 
 def using_sort(array)
-
-end
-
-def using_reverse(array)
-
-end
-
-def using_first(array)
-
-end
-
-def using_last(array)
-
-end
-
-def using_size(array)
-
+  
 end
